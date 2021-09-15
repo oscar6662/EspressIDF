396a3552279b13e01843e41d0bf8fc5fbbb9ee5d
Óskar Helgi <oscar.berenguer6@gmail.com>
2021-09-15 16:51:08 +0000

    mac as well

--- main/main.c
+++ main/main.c
@@ -21,15 +21,24 @@ void serial_out(const char* string) {
 	printf(msg_buffer);
 	fflush(stdout);
 }
+void print_mac(const unsigned char *mac) {
+	printf("%02X:%02X:%02X:%02X:%02X:%02X", mac[0],mac[1],mac[2],mac[3],mac[4],mac[5]);
+}
 
 void check_what_came_in (const char * string) {
+ 	unsigned char mac_base[6] = {0};
+    esp_efuse_mac_get_default(mac_base);
+    printf("MAC Address: ");
+    print_mac(mac_base);
 	char ping[4] = "ping";
 	char pong[4] = "pong";
+	char mac[3] = "mac"; 
 	for (int i = 0; i < 3; i ++) {
 		if(string[i] != ping[i]) {
 			return;
 		} 
 	}
+
 	serial_out(pong);
 	serial_out("");
 }
